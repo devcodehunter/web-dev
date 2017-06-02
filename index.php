306fa62849b14857c8ea5d7ff6251b6432658251
Shawon Ahmed <hr111222333444@gmail.com>
2017-06-02 19:54:58 +0600

    add all files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Web-Dev</title>
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+
+	<body>
+		
+		<h1>Click a list item to replace this text</h1>
+		<div id="res"></div>
+		
+		<button onclick="">Add New Item</button>
+		<ul id="list-item">
+			<li>First Item</li>
+			<li>Second Item</li>
+			<li>Third Item</li>
+			<li>Four Item</li>
+			<li>Five Item</li>
+		</ul>
+		<script src="js/main.js"></script>
+	</body>
+</html>
+
