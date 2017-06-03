7b21e562e75ba84893ce4163de5e1181022932f1
G M Morshed <shawonahmed78@gmail.com>
2017-06-04 03:49:03 +0600

    file added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 
+var result=document.getElementById('res');
+var employee={
+	'name': "Shawon Ahmed",
+	'email': "shawonahmed78@gmail.com",
+	'phone': 01686138967,
+	'zip':1219,
+	'street':"398/C, Stone Shurma Mahal, Khilgaon",
+	'gender':"Male"
+};
 
+result.innerHTML=employee.name;
